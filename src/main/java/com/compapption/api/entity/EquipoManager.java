acf81bb032030b5c3ae4d66c3204d32da5075580
MarioNaya <marionayadeluis@gmail.com>
2026-02-06 14:53:32 +0100

    Completada entidad EquipoManager.java

--- src/main/java/com/compapption/api/entity/EquipoManager.java
+++ src/main/java/com/compapption/api/entity/EquipoManager.java
@@ -2,6 +2,9 @@ package com.compapption.api.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "equipo_manager", uniqueConstraints = @UniqueConstraint(columnNames = {"equipo_id", "competicion_id", "usuario_id"}))
@@ -20,5 +23,15 @@ public class EquipoManager {
     @JoinColumn(name = "equipo_id", nullable = false)
     private Equipo equipo;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "competicion_id", nullable = false)
+    private Competicion competicion;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @CreationTimestamp
+    @Column(name = "fecha_asignacion", updatable = false)
+    private LocalDateTime fechaAsigacion;
 }
