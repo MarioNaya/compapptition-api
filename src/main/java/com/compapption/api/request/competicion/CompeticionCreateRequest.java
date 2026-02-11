2c310ba6cb012264b420683ab92fb70fdf61c40d
MarioNaya <marionayadeluis@gmail.com>
2026-02-11 10:08:51 +0100

    Creadas requests de competicion.

---
+++ src/main/java/com/compapption/api/request/competicion/CompeticionCreateRequest.java
@@ -0,0 +1,52 @@
+package com.compapption.api.request.competicion;
+
+import com.compapption.api.entity.ConfiguracionCompeticion;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CompeticionCreateRequest {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(max = 100, message = "El nombre no puede exceder los 100 caracteres")
+    private String nombre;
+
+    private String descripcion;
+
+    @NotNull(message = "El deporte es obligatorio")
+    private Long deporteId;
+
+    private Boolean publica;
+    private Boolean inscripcionAbierta;
+    private Boolean estadisticasActivas;
+
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+
+    private ConfiguracionRequest configuracion;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ConfiguracionRequest {
+
+        private Integer puntosVictoria;
+        private Integer puntosEmpate;
+        private Integer puntosDerrota;
+        private ConfiguracionCompeticion.FormatoCompeticion formato;
+        private Integer numEquiposPlayoff;
+        private Integer partidosEliminatoria;
+    }
+
+}
---
+++ src/main/java/com/compapption/api/request/competicion/CompeticionUpdateRequest.java
@@ -0,0 +1,40 @@
+package com.compapption.api.request.competicion;
+
+import com.compapption.api.entity.Competicion;
+import com.compapption.api.entity.ConfiguracionCompeticion;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CompeticionUpdateRequest {
+
+    @Size(max = 100, message = "El nombre no puede exceder los 100 caracteres")
+    private String nombre;
+
+    private String descripcion;
+    private Boolean publica;
+    private Boolean inscripcionAbierta;
+    private Boolean estadisticasActivas;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private Competicion.EstadoCompeticion estado;
+
+    private ConfiguracionUpdaterequest configuracion;
+
+    public static class ConfiguracionUpdaterequest {
+        private Integer puntosVictoria;
+        private Integer puntosEmpate;
+        private Integer puntosDerrota;
+        private ConfiguracionCompeticion.FormatoCompeticion formato;
+        private Integer numEquiposPlayOff;
+        private Integer partidosEliminatoria;
+    }
+}
